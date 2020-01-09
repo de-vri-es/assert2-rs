c0ab55ba371aa6f0c3568802b08c78c9ac29e6f8
Maarten de Vries <maarten@de-vri.es>
2020-01-09 02:13:12 +0100

    Add rudimentary documentation.

---
+++ README.md
@@ -0,0 +1,6 @@
+# assert2
+
+`assert!(...)` and `check!(...)` macros inspired by Catch2.
+
+This crate is currently a work in progress.
+It relies on a nightly compiler with the `proc_macro_hygiene`, `proc_macro_span` and `specialization` features.
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 #![feature(proc_macro_hygiene)]
 #![feature(specialization)]
 
+//! `assert!(...)` and `check!(...)` macros inspired by Catch2.
+//!
+//! This crate is currently a work in progress.
+//! It relies on a nightly compiler with the `proc_macro_hygiene`, `proc_macro_span` and `specialization` features.
+
 mod maybe_debug;
 
 #[doc(hidden)]
