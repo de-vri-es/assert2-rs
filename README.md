657de8695e3da44526f5d276715bc1a832beffdc
Maarten de Vries <maarten@de-vri.es>
2020-01-09 02:36:25 +0100

    Add example to documentation.

--- README.md
+++ README.md
@@ -4,3 +4,19 @@
 
 This crate is currently a work in progress.
 It relies on a nightly compiler with the `proc_macro_hygiene`, `proc_macro_span` and `specialization` features.
+
+As a user of the crate, you also need to enable the `proc_macro_hygiene` feature.
+
+## Example
+```rust
+#![feature(proc_macro_hygiene)]
+use assert2::check;
+
+let mut vec = Vec::new();
+vec.push(12);
+
+check!(vec.len() == 2);
+check!(&vec == &vec![10]);
+```
+
+![Example output](https://github.com/de-vri-es/assert2-rs/blob/v0.0.3/example.png)
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,24 @@
 //!
 //! This crate is currently a work in progress.
 //! It relies on a nightly compiler with the `proc_macro_hygiene`, `proc_macro_span` and `specialization` features.
+//!
+//! As a user of the crate, you also need to enable the `proc_macro_hygiene` feature.
+//!
+//! # Example
+//! ```should_panic
+//! #![feature(proc_macro_hygiene)]
+//! use assert2::check;
+//!
+//! # fn main() {
+//! let mut vec = Vec::new();
+//! vec.push(12);
+//!
+//! check!(vec.len() == 2);
+//! check!(&vec == &vec![10]);
+//! # }
+//! ```
+//!
+//! ![Example output](https://github.com/de-vri-es/assert2-rs/blob/v0.0.3/example.png)
 
 pub use assert2_macros::assert;
 pub use assert2_macros::check;
