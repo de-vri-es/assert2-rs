527982c62d01999ed57363a3cdc0d043a2ff6e5d
Maarten de Vries <maarten@de-vri.es>
2020-01-09 02:06:15 +0100

    Document internal FailGuard type.

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,10 @@ pub mod print;
 pub use check_macros::assert;
 pub use check_macros::check;
 
+/// Scope guard to panic when a check!() fails.
+///
+/// The panic is done by a lambda passed to the guard,
+/// so that the line information points to the check!() invocation.
 #[doc(hidden)]
 pub struct FailGuard<T: FnMut()>(pub T);
 
