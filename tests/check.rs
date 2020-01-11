831764129cce58af6f9f8b64886c3bb90b4a4731
Maarten de Vries <maarten@de-vri.es>
2020-01-11 15:49:53 +0100

    Add tests for all forms of the macros.

--- tests/check.rs
+++ tests/check.rs
@@ -1,25 +1,57 @@
 #![feature(proc_macro_hygiene)]
 
 use assert2::check;
+use assert2::assert;
 
 #[derive(Eq, PartialEq)]
 struct Foo(u32);
 
 #[test]
-fn pass() {
-	assert2::check!(1 == 1);
-	assert2::check!(true && true);
-	assert2::check!(!(false && true));
+fn check_pass() {
+	check!(1 == 1);
+	check!(1 == 1, "{}", "math broke");
+	check!(1 == 1, "{}", "math broke",);
+	check!(true && true);
+	check!(true && true, "{}", "logic broke");
+	check!(true && true, "{}", "logic broke",);
+	check!(let Ok(10) = Result::<i32, i32>::Ok(10));
+	check!(let Ok(10) = Result::<i32, i32>::Ok(10), "{}", "rust broke");
+	check!(let Ok(10) = Result::<i32, i32>::Ok(10), "{}", "rust broke",);
 }
 
 #[test]
-#[should_panic]
-fn check_panic1() {
-	check!(1 == 2);
+fn assert_pass() {
+	assert!(1 == 1);
+	assert!(1 == 1, "{}", "math broke");
+	assert!(1 == 1, "{}", "math broke",);
+	assert!(true && true);
+	assert!(true && true, "{}", "logic broke");
+	assert!(true && true, "{}", "logic broke",);
+	assert!(let Ok(10) = Result::<i32, i32>::Ok(10));
+	assert!(let Ok(10) = Result::<i32, i32>::Ok(10), "{}", "rust broke");
+	assert!(let Ok(10) = Result::<i32, i32>::Ok(10), "{}", "rust broke",);
 }
 
-#[test]
-#[should_panic]
-fn check_panic2() {
-	check!(true && false);
+macro_rules! test_panic {
+	($name:ident, $($expr:tt)*) => {
+		#[test]
+		#[should_panic]
+		fn $name() {
+			$($expr)*;
+		}
+	}
 }
+
+test_panic!(panic_check1, check!(1 == 2));
+test_panic!(panic_check2, check!(1 == 2, "{}", "math broke"));
+test_panic!(panic_check3, check!(true && false));
+test_panic!(panic_check4, check!(true && false, "{}", "logic broke"));
+test_panic!(panic_check5, check!(let Ok(_) = Result::<i32, i32>::Err(10)));
+test_panic!(panic_check6, check!(let Ok(_) = Result::<i32, i32>::Err(10), "{}", "rust broke"));
+
+test_panic!(panic_assert1, assert!(1 == 2));
+test_panic!(panic_assert2, assert!(1 == 2, "{}", "math broke"));
+test_panic!(panic_assert3, assert!(true && false));
+test_panic!(panic_assert4, assert!(true && false, "{}", "logic broke"));
+test_panic!(panic_assert5, assert!(let Ok(_) = Result::<i32, i32>::Err(10)));
+test_panic!(panic_assert6, assert!(let Ok(_) = Result::<i32, i32>::Err(10), "{}", "rust broke"));
