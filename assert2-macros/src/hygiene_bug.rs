2822a58e1123af70b6596bdb8fa812d2866e5132
Maarten de Vries <maarten@de-vri.es>
2020-07-04 20:59:06 +0200

    Document what hygiene fix does and why it exists.

--- assert2-macros/src/hygiene_bug.rs
+++ assert2-macros/src/hygiene_bug.rs
@@ -1,20 +1,27 @@
 use proc_macro::{Delimiter, Group, TokenStream, TokenTree};
 
-pub fn fix(ts: TokenStream) -> TokenStream {
-	ts.into_iter()
-		.map(|t| match t {
-			TokenTree::Group(g) => {
+/// Fix-up a token stream to work around a hygiene bug in the Rust compiler.
+///
+/// This turns all none-delimited groups into parenthesis,
+/// so that their precedence remains correct.
+///
+/// See https://github.com/rust-lang/rust/issues/74036
+/// and https://github.com/rust-lang/rust/issues/67062
+pub fn fix(tokens: TokenStream) -> TokenStream {
+	tokens.into_iter()
+		.map(|token| match token {
+			TokenTree::Group(group) => {
 				let mut fixed = Group::new(
-					match g.delimiter() {
+					match group.delimiter() {
 						Delimiter::None => Delimiter::Parenthesis,
-						d => d,
+						delimiter => delimiter,
 					},
-					fix(g.stream()),
+					fix(group.stream()),
 				);
-				fixed.set_span(g.span());
+				fixed.set_span(group.span());
 				TokenTree::Group(fixed)
 			}
-			t => t,
+			token => token,
 		})
 		.collect()
 }
