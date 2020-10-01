45e114875d175643f90d71177b3f2a0d68f81652
Maarten de Vries <maarten@de-vri.es>
2020-10-02 01:58:29 +0200

    Update example with new macros.

--- examples/images.rs
+++ examples/images.rs
@@ -1,13 +1,25 @@
-use assert2::check;
-use assert2::let_assert;
+use assert2::{check, check_scope, capture, info, let_assert};
 use std::fs::File;
 use std::io::ErrorKind;
 
 fn main() {
-	check!(6 + 1 <= 2 * 3);
-	check!(true && false);
-	check!(let Ok(_) = File::open("/non/existing/file"));
+	check_scope! {
+		check!(6 + 1 <= 2 * 3);
+		check!(true && false);
 
-	let_assert!(Err(e) = File::open("/non/existing/file"));
-	check!(e.kind() == ErrorKind::PermissionDenied);
+		{
+			info!("opening file");
+			check!(let Ok(_) = File::open("/non/existing/file"));
+		}
+
+		let_assert!(Err(e) = File::open("/non/existing/file"));
+		check!(e.kind() == ErrorKind::PermissionDenied);
+
+		let data = [2, 4, 6, 8, 9];
+		for i in 0..data.len() {
+			capture!(i);
+			capture!(data[i]);
+			check!(data[i] % 2 == 0);
+		}
+	}
 }
